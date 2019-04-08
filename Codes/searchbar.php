c389cc646ae2fad3d5cd1fde1188f83b36285398
AnushaVanama <AnushaVanama@users.noreply.github.com>
2019-04-08 10:33:50 -0400

    Updated Latest

--- Codes/searchbar.php
+++ Codes/searchbar.php
@@ -1,4 +1,7 @@
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Load an icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <head>
 <style>
 table, th, td {
@@ -6,6 +9,62 @@ table, th, td {
 }
 </style>
 </head>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+.navbar {
+  width: 100%;
+  background-color: #216;
+  overflow: auto;
+  position: relative;
+ 
+ }
+.navbar a {
+  float: right;
+  padding: 10px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+}
+.navbar a:hover {
+  background-color: #225;
+}
+.active {
+  background-color: #216;
+}
+@media only screen and (max-width: 1044px) {
+  .navbar a {
+    float: none;
+    display: block;
+  }
+}
+.dealer-logo{
+    text-align: Left;
+	background-color: transparent;
+	text-decoration: none;
+	padding: 1px;
+  
+}
+.SearchBar {     
+     top: 355px;
+     left: 575px;
+}
+.SearchBar {
+     height: 30px;
+     width: 500px;
+}
+</style>
+<body>
+<div class="dealer-logo">
+<center>
+<img class="dealer-logo" 
+src="..\Images\Logo.png" alt="HTML5 Icon" style="width:auto;height:75px;"> 
+<img src="..\Images\Name.png" alt="HTML5 Icon" style="display:inline-block;width:auto;height:75px;" > 
+</center>
+</div>
+</div>
+<div class="navbar">
+   <a class="active" href="index.php"><i class="fa fa-fw fa-home"></i>Home</a>
+</div>
 </html>
 <?php
 
